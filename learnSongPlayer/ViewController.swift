beeb40347fb76150b9af0adb3977f4f99c6d1c0c
Nova Arisma <novaarisma95@gmail.com>
2019-10-15 16:48:40 +0700

    Initial Commit

---
+++ learnSongPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learnSongPlayer
+//
+//  Created by Nova Arisma on 10/15/19.
+//  Copyright © 2019 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
